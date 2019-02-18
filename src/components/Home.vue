47db3567feb7614e66d051e0b328c58bd7202442
dranzture <polatcoban89@hotmail.com>
2019-02-18 17:59:28 -0600

    homepage last

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,8 @@
   <div>
     <div id="home-main" class="wrapper text-center">
       <img id="profile-pic" src="https://picsum.photos/500/500">
-      <p class="pt-2">Welcome to Polat Coban's Portfolio!</p>
-      <p>San Antonio, TX</p>
+      <p class="pt-5 text-white">Welcome to Polat Coban's Portfolio!</p>
+      <p class="text-white">San Antonio, TX</p>
     </div>
   </div>
 </template>
@@ -19,14 +19,14 @@ export default {
 </script>
 
 <style>
- @import url('https://fonts.googleapis.com/css?family=Rajdhani');
- #home-main{
-  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#87e0fd+0,53cbf1+56,05abe0+100;Blue+3D+%23+16 */
-  background: #87e0fd; /* Old browsers */
-  background: -moz-linear-gradient(top, #87e0fd 0%, #53cbf1 56%, #05abe0 100%); /* FF3.6-15 */
-  background: -webkit-linear-gradient(top, #87e0fd 0%,#53cbf1 56%,#05abe0 100%); /* Chrome10-25,Safari5.1-6 */
-  background: linear-gradient(to bottom, #87e0fd 0%,#53cbf1 56%,#05abe0 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#87e0fd', endColorstr='#05abe0',GradientType=0 ); /* IE6-9 */
+@import url('https://fonts.googleapis.com/css?family=Julius+Sans+One');
+#home-main{
+  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#2cafe3+0,49c0f0+0 */
+  background: #2cafe3; /* Old browsers */
+  background: -moz-linear-gradient(top, #2cafe3 0%, #49c0f0 0%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(top, #2cafe3 0%,#49c0f0 0%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to bottom, #2cafe3 0%,#49c0f0 0%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2cafe3', endColorstr='#49c0f0',GradientType=0 ); /* IE6-9 */
   position: absolute; top: 0; right: 0; bottom: 0; left: 0;
 }
 #profile-pic{
@@ -34,7 +34,8 @@ export default {
   padding-top: 10em;
 }
 p{
-  font-family: 'Rajdhani', sans-serif;
+  font-family: 'Julius Sans One', sans-serif;
   font-size: 32px;
+  
 }
 </style>
